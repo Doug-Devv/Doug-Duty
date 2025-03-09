67572bd44c088ad47be7bdf2bef907e15e27c0ac
Douggie 🐱 <okrzesikbankston@gmail.com>
2025-03-09 00:22:17 -0600

    update  fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Advanced Duty Script with ox_lib and Discord ACE Perms'
+version '1.0.0'
+
+shared_script '@ox_lib/init.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+dependencies {
+    'ox_lib'
+}
